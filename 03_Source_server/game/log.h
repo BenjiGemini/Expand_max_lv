ee1181f473d8357025134ddb072ab4c196eb6545
Benji <88248649+BenjiGemini@users.noreply.github.com>
2022-03-13 13:21:43 +0100

    Create log.h

---
+++ 03_Source_server/game/log.h
@@ -0,0 +1,9 @@
+---Rechercher :
+#######################################################################
+
+void		LoginLog(bool isLogin, DWORD dwAccountID, DWORD dwPID, BYTE bLevel, BYTE bJob, DWORD dwPlayTime);
+
+Remplacer :
+#######################################################################
+
+void		LoginLog(bool isLogin, DWORD dwAccountID, DWORD dwPID, int bLevel, BYTE bJob, DWORD dwPlayTime);
