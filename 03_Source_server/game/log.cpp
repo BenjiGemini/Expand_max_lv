01eb77e9a248804ea991fc31a32d93de3b1d5c04
Benji <88248649+BenjiGemini@users.noreply.github.com>
2022-03-13 13:23:30 +0100

    Create log.cpp

---
+++ 03_Source_server/game/log.cpp
@@ -0,0 +1,9 @@
+---Chercher :
+#######################################################################
+
+void LogManager::LoginLog(bool isLogin, DWORD dwAccountID, DWORD dwPID, BYTE bLevel, BYTE bJob, DWORD dwPlayTime)
+
+Remplacer :
+#######################################################################
+
+void LogManager::LoginLog(bool isLogin, DWORD dwAccountID, DWORD dwPID, int bLevel, BYTE bJob, DWORD dwPlayTime)
