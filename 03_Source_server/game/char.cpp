751903856a334644a1136878302d5de5f77d93df
Benji <88248649+BenjiGemini@users.noreply.github.com>
2022-03-13 13:05:58 +0100

    Create char.cpp

---
+++ 03_Source_server/game/char.cpp
@@ -0,0 +1,9 @@
+Rechercher :
+#######################################################################
+
+void CHARACTER::SetLevel(BYTE level)
+
+Remplacer par :
+#######################################################################
+
+void CHARACTER::SetLevel(int level)
